cf5cf8022e6786f01086841d68e9f987401ead52
optimisme <optimisme@gmail.com>
2026-03-02 18:51:18 +0100

    .

--- game_example/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('renders cupertino app shell', (WidgetTester tester) async {
-    await tester.pumpWidget(const CupertinoApp(home: SizedBox.shrink()));
-    expect(find.byType(CupertinoApp), findsOneWidget);
-  });
-}
