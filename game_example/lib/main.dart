25712275019deb0e786567b9c73fa676eab1a259
optimisme <optimisme@gmail.com>
2026-03-02 13:02:14 +0100

    Set game example window title with window_manager

--- game_example/lib/main.dart
+++ game_example/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
 import 'app_data.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  const WindowOptions windowOptions = WindowOptions(
+    title: 'Game Example',
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+    await windowManager.setTitle('Game Example');
+  });
 
   runApp(
     ChangeNotifierProvider(
